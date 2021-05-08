54070419e50c80b9e15b4e3ae189de3fd5b18eb2
Ramkumar G <ramkumar.g.0420@gmail.com>
2021-05-08 15:59:08 +0530

    Question validation moved to ValidationUtil

--- src/main/java/in/ramkumar/question/QuestionManager.java
+++ src/main/java/in/ramkumar/question/QuestionManager.java
@@ -2,19 +2,22 @@ package in.ramkumar.question;
 
 import java.util.ArrayList;
 
+import in.ramkumar.util.ValidationUtil;
+
 public class QuestionManager {
 
 	private static ArrayList<Question> questionList = new ArrayList<Question>();
 
 	/**
-	 * This method validates the question, if it is a valid question then it will be
-	 * added to questionList.
+	 * Add Question. This method validates the question, if it is a valid question
+	 * then it will be added to questionList.
+	 * 
 	 * @param question
-	 * @return
+	 * @return Returns true iff it is a valid Question object.
 	 */
 	public static boolean addQuestion(Question question) {
 		boolean validQuestion = false;
-		if (validateQuestion(question) && validateDescription(question)) {
+		if (ValidationUtil.validateQuestion(question) && ValidationUtil.validateDescription(question)) {
 			questionList.add(question);
 			validQuestion = true;
 		}
@@ -22,64 +25,17 @@ public class QuestionManager {
 	}
 
 	/**
-	 * This method returns list of questions.
-	 * @return
+	 * @return Returns list of questions.
 	 */
 	public static ArrayList<Question> getAllQuestion() {
 		return questionList;
 	}
 
 	/**
-	 * This method validates the given question. Question should not be null, empty,
-	 * and also the length of the question should not be > 300
-	 * 
-	 * @param questionObject
-	 * @return
+	 * @return Returns number of questions in the question list.
 	 */
-	public static boolean validateQuestion(Question questionObject) {
-		String question = questionObject.getQuestion();
-		boolean validQuestion = false;
-		Integer numberOfCharactersForQuestion = 300;
-		try {
-			int questionLength = getLength(question);
-			if (question != null && question.trim() != "" && questionLength <= numberOfCharactersForQuestion) {
-				validQuestion = true;
-			}
-		} catch (Exception e) {
-			validQuestion = false;
-		}
-		return validQuestion;
-	}
-
-	/**
-	 * This method validates the given description. Description should not be null,
-	 * empty, and also the length of the description should not be > 600
-	 * @param questionObject
-	 * @return
-	 */
-	public static boolean validateDescription(Question questionObject) {
-		boolean validDescription = false;
-		String description = questionObject.getDescription();
-		Integer numberOfCharactersForDescription = 600;
-		try {
-			int descriptionLength = getLength(description);
-			if (description != null && description.trim() != ""
-					&& descriptionLength <= numberOfCharactersForDescription) {
-				validDescription = true;
-			}
-		} catch (Exception e) {
-			validDescription = false;
-		}
-		return validDescription;
-	}
-
-	/**
-	 * This method returns the length of the given string.
-	 * @param string
-	 * @return
-	 */
-	public static int getLength(String string) {
-		return string.trim().length();
+	public static int getNumberOfQuestion() {
+		return questionList.size();
 	}
 
 }
